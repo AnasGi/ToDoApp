41108699e1ef3b7b211deffcd653b33c054a4d77
Anas Boussalem <anassboussalem8@gmail.com>
2023-12-23 14:26:50 +0100

    structure html 1

--- index.php
+++ index.php
@@ -19,3 +19,29 @@
         $tache->bindValue(":p3" , strtotime("now"));
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do App</title>
+</head>
+<body>
+    <div>
+        <div><h1>Todo List</h1></div>
+        <div>
+            <form method="post">
+                <input type="text" name="task" placeholder="Task Title"/>
+                <input type="submit" name="add" value="Add" />
+            </form>
+            <div>
+                <?php
+
+                ?>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
